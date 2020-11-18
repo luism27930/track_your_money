3c3594dbc24cd48841261443664ecd84feca66a9
unknown <lmirandaurena27@gmail.com>
2020-11-17 22:37:20 -0600

    login with google

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -28,30 +28,21 @@ class GoogleController extends Controller
     public function handleGoogleCallback()
     {
         try {
-      
             $user = Socialite::driver('google')->user();
-       
             $finduser = User::where('google_id', $user->id)->first();
-       
             if($finduser){
-       
                 Auth::login($finduser);
-      
                 return redirect()->intended('dashboard');
-       
             }else{
                 $newUser = User::create([
                     'name' => $user->name,
                     'email' => $user->email,
                     'google_id'=> $user->id,
-                    'password' => encrypt('123456dummy')
+                    'password' => encrypt('123456')
                 ]);
-      
                 Auth::login($newUser);
-      
                 return redirect()->intended('dashboard');
             }
-      
         } catch (Exception $e) {
             dd($e->getMessage());
         }
